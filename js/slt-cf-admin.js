c2f3e86add0a65d44921216b4e032a0f39b38e91
gyrus <gyrus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-26 11:39:04 +0000

    0.8.4 release
    git-svn-id: https://plugins.svn.wordpress.org/developers-custom-fields/tags/0.8.4@762389 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/slt-cf-admin.js
@@ -0,0 +1,180 @@
+/* SLT Custom Fields JavaScript
+*********************************************************************/
+
+jQuery( document ).ready( function($) {
+	var	clean07 = $( 'a#slt-cf-dismiss_alert-07-cleanup' ),
+		cp = $( '.slt-cf-color-preview' ),
+		s = $( '.slt-cf-sortable' ),
+		i, box;
+
+	/* Move meta boxes above content editor
+	*****************************************************************/
+	if ( typeof slt_cf_metaboxes_above_content != "undefined" ) {
+		for ( i in slt_cf_metaboxes_above_content ) {
+			box = $( 'div#' + slt_cf_metaboxes_above_content[ i ] ).detach();
+			box.insertAfter( '#titlediv' );
+		}
+	}
+
+	/* Color preview
+	 *****************************************************************/
+	if ( cp.length ) {
+		cp.each( function() {
+			var $this = $( this );
+			var name = $this.attr( 'id' ).replace( 'slt-cf-color-preview_', '' );
+			slt_cf_color_preview_update( name );
+			$( '[name=' + name + ']' ).on( 'change', function() {
+				slt_cf_color_preview_update( $( this ).attr( 'name' ) );
+			});
+		});
+	}
+
+	/* Sortable
+	 *****************************************************************/
+	if ( s.length ) {
+		// Store initial order
+		s.each( function() {
+			slt_cf_store_sortable_order( $( this ) );
+		});
+		// Initialize sortability
+		s.sortable({
+			stop: function( e, ui ) {
+				// When sorting stops, store order
+				slt_cf_store_sortable_order( ui.item.parents( '.slt-cf-sortable' ) );
+			}
+		});
+	}
+
+	/* Handling notices
+	*****************************************************************/
+
+	// 0.7 cleanup
+	if ( clean07.length ) {
+		clean07.click( function() {
+			$.post(
+				slt_custom_fields.ajaxurl,
+				{
+					'action': 'slt_cf_update_option',
+					'key': 'alert-07-cleanup',
+					'value': 0,
+					'update-option-nonce': slt_custom_fields.update_option_nonce
+				},
+				function( data ) {
+					if ( data == 'updated' ) {
+						$( 'a#slt-cf-dismiss_alert-07-cleanup' ).parents( 'div#message' ).fadeOut( 600, function() { $( this ).remove(); } );
+					} else {
+						alert( slt_custom_fields.update_option_fail );
+					}
+				}
+			);
+			return false;
+		});
+	}
+
+	/* Cloning
+	****************************************************************
+	if ( $( "p.slt-cf-clone-field a" ).length ) {
+		$( 'p.slt-cf-clone-field a' ).click( function() {
+
+			// Initialize
+			var masterFieldDiv, fieldName, fieldNameParts, fieldClassPrefix, lastCloneFieldName, newField, newFieldNumber, newFieldName;
+			fieldClassPrefix = 'slt-cf-field_';
+			masterFieldDiv = $( this ).parents( '.slt-cf' );
+			fieldName = getValueFromClass( masterFieldDiv, fieldClassPrefix );
+
+			// Store the field name for what is currently the last clone
+			lastCloneFieldName = getValueFromClass( $( '.' + fieldClassPrefix + fieldName ).last(), fieldClassPrefix );
+
+			// Clone the master field, placing the clone at the end of all clones
+			$( '.' + fieldClassPrefix + fieldName ).last().after( masterFieldDiv.clone( true ) );
+
+			// Get a handle for it and hide it straight away
+			newField = $( '.' + fieldClassPrefix + fieldName ).last();
+			newField.hide();
+
+			// Determine new number / ID
+			fieldNameParts = lastCloneFieldName.split( '_' );
+			newFieldNumber = ( fieldNameParts.length > 1 ) ? ( fieldNameParts[1] + 1 ) : 2;
+			newFieldName = fieldNameParts[0] + '_' + newFieldNumber;
+
+			// Remove / add classes
+			newField.removeClass( fieldClassPrefix + fieldName ).addClass( 'clone ' + fieldClassPrefix + newFieldName );
+
+			// Adjust label and input ????
+			newField.children( 'label' ).attr( 'for', newFieldName ).append( ' #' + newFieldNumber );
+
+			// Remove unnecessary elements
+			newField.children( '.description, p.slt-cf-clone-field' ).remove();
+
+			// Reveal new field
+			newField.slideDown();
+
+			return false;
+		});
+	}
+
+	// Get a value from a class
+	function getValueFromClass( obj, prefix ) {
+		var classes, value, i;
+		classes = value = "";
+		classes = obj.attr( "class" );
+		classes = classes.split( " " );
+		for ( i = 0; i < classes.length; i++ ) {
+			if ( classes[i].length > prefix.length && classes[i].substr( 0, prefix.length ) == prefix ) {
+				value = classes[i].substr( prefix.length );
+				break;
+			}
+		}
+		return value;
+	}*/
+
+});
+
+
+/**
+ * Store the order of sortable checkboxes
+ *
+ * @since	0.8.4
+ * @param	object	el	The parent sortable element
+ * @return	void
+ */
+function slt_cf_store_sortable_order( el ) {
+	var o = [];
+	el.children( '.slt-cf-multifield' ).each( function() {
+		var n = jQuery( this ).find( 'input' ).attr( 'name' ).split( '_' );
+		o.push( n.pop() );
+	});
+	el.siblings( 'input.slt-cf-sortable-order' ).val( o.join( ',' ) );
+}
+
+
+/**
+ * Update a color preview
+ *
+ * @since	0.8.3
+ * @param	object	n	The name of the field with the preview
+ * @return	void
+ */
+function slt_cf_color_preview_update( n ) { jQuery( function($) {
+	var val;
+	var f = $( '[name=' + n + ']' );
+	var cp = $( '#slt-cf-color-preview_' + n );
+	if ( f.is( 'select' ) ) {
+		// Select drop-down
+		val = $( 'option:selected', f ).val();
+		if ( ! val ) {
+			val = $( 'option:first', f ).val();
+		}
+	} else if ( f.length > 1 && $( f[0] ).is( 'input' ) && $( f[0] ).attr( 'type' ) == 'radio' ) {
+		// Radio
+		val = $( '[name=' + n + ']:checked' ).val();
+	} else {
+		// Default
+		val = f.val();
+	}
+	if ( typeof val != 'undefined' && /^[0-9a-f]{3,6}$/i.test( val ) ) {
+		cp.css( 'background-color', '#' + val ).html( '&nbsp' );
+	} else {
+		cp.css( 'background-color', 'none' ).html( '?' );
+	}
+}); }
---
+++ slt-cf-save.php
@@ -0,0 +1,158 @@
+<?php
+
+/* Save custom field values
+***************************************************************************************/
+
+/**
+ * Save custom field values
+ *
+ * @since	0.1
+ * @param	string	$request_type	'post' | 'attachment' | 'user' (corresponds to $type in slt_cf_register_box)
+ * @param	integer	$object_id		ID of object being edited
+ * @param	object	$object			The object being edited
+ * @param	array	$extras			Any extras, e.g. $attachment passed by attachment_fields_to_save filter
+ * @return	mixed
+ */
+function slt_cf_save( $request_type, $object_id, $object, $extras = array() ) {
+	global $slt_custom_fields;
+
+	// Metadata type
+	if ( $request_type == 'attachment' )
+		$metadata_type = 'post';
+	else
+		$metadata_type = $request_type;
+
+	// Loop through boxes
+	foreach ( $slt_custom_fields['boxes'] as $box ) {
+
+		// Check meta box nonce
+		$nonce_prefix = '';
+		if ( $request_type == 'post' || ( SLT_CF_WP_IS_GTE_3_5 && $request_type == 'attachment' ) )
+			$nonce_prefix = slt_cf_prefix( $request_type ) . $box['id'];
+
+		if ( ! $nonce_prefix || ( isset( $_POST[ $nonce_prefix . '_wpnonce' ] ) && wp_verify_nonce( $_POST[ $nonce_prefix . '_wpnonce' ], $nonce_prefix . '_save' ) ) ) {
+
+			// Loop through fields
+			foreach ( $box['fields'] as $field ) {
+				// Skip notices
+				if ( $field['type'] == 'notice' )
+					continue;
+
+				// Initialize
+				$field_name = slt_cf_prefix( $request_type ) . $field['name'];
+
+				// Process the submitted value
+				$value = null;
+				$update = true;
+
+				if ( $field['type'] == 'checkboxes' ) {
+
+					/* Multiple checkboxes - gather values into array
+					*************************************************************/
+					$value = array();
+					foreach ( $field['options'] as $opt_key => $opt_value ) {
+						if ( isset( $_POST[ $field_name . '_' . $opt_value ] ) )
+							$value[] = $opt_value;
+					}
+
+					// Store order for sortable
+					if ( $field['sortable'] && isset( $_POST[ $field_name . '_order' ] ) ) {
+						update_metadata( $metadata_type, $object_id, $field_name . '_order', $_POST[ $field_name . '_order' ] );
+					}
+
+				} else if ( $field['type'] == 'checkbox' ) {
+
+					/* Single checkbox - set value to 1 or 0
+					*************************************************************/
+					$value = ( isset( $_POST[ $field_name ] ) ? '1' : '0' );
+
+				} else if ( isset( $_POST[ $field_name ] ) || ( $request_type == 'attachment' && isset( $_POST['attachments'][$object_id][$field_name] ) ) ) {
+
+					/* Other field types
+					*************************************************************/
+					if ( isset( $_POST['attachments'][$object_id][$field_name] ) )
+						$value =  $_POST['attachments'][$object_id][$field_name];
+					else
+						$value = $_POST[ $field_name ];
+
+					// Deal with string inputs
+					if ( in_array( $field['type'], array( 'text', 'textarea', 'textile', 'wysiwyg' ) ) ) {
+
+						// Basic trim
+						$value = trim( $value );
+
+						if ( $field['type'] == "textile" ) {
+
+							// Textile: strip all tags, then format
+							$value = wp_kses( $value, array() );
+							$value = slt_cf_simple_formatting( $value, 'html', $field['autop'] );
+
+						} else if ( ! current_user_can( 'unfiltered_html' ) ) {
+
+							// For users that can't submit unfiltered HTML...
+
+							// Are there any tags defined as being allowed?
+							if ( count( $field['allowed_html'] ) ) {
+								// Strip all tags except those allowed
+								$value = wp_kses( $value, $field['allowed_html'] );
+							} else if ( count( $field['allowtags'] ) ) {
+								// Deprecated
+								$value = strip_tags( $value, '<' . implode( '><', $field['allowtags'] ) . '>' );
+							} else if ( in_array( $field['type'], array( 'text', 'textarea' ) ) ) {
+								// If no tags are allowed, for text and textarea, strip all HTML
+								$value = wp_kses( $value, array() );
+							} else if ( $field['type'] == 'wysiwyg' ) {
+								// WYSIWYG: default to allow standard post tags
+								$value = wp_kses_post( $value );
+							}
+
+						}
+
+						// Auto-paragraphs for WYSIWYG and other fields with autop set
+						if ( $field['type'] == 'wysiwyg' || $field['autop'] )
+							$value = wpautop( $value );
+
+					}
+
+				} // Field type if
+
+				// Save meta entry
+				if ( $update ) {
+
+					// Apply filters to value first
+					$value = apply_filters( 'slt_cf_pre_save_value', $value, $request_type, $object_id, $object, $field );
+
+					// Run save actions
+					do_action( 'slt_cf_pre_save', $value, $request_type, $object_id, $object, $field );
+
+					// Separate fields?
+					if ( ! $field['single'] && ( $field['type'] == 'checkboxes' || ( $field['type'] == 'select' && $field['multiple'] ) ) ) {
+						// Remove all old values
+						delete_metadata( $request_type, $object_id, $field_name );
+						// Add each new value separately, if there are values
+						if ( $value ) {
+							foreach ( $value as $value_item )
+								add_metadata( $metadata_type, $object_id, $field_name, $value_item, false );
+						}
+					} else if ( $value == '' ) {
+						// Delete field if it exists (and don't create it if it doesn't!)
+						delete_metadata( $metadata_type, $object_id, $field_name );
+					} else {
+						// Update single field
+						update_metadata( $metadata_type, $object_id, $field_name, $value );
+					}
+
+				}
+
+			} // Fields foreach
+
+		} // Nonce check if
+
+	} // Boxes foreach
+
+	// Return $post for attachments pre-3.5 (it's a filter, not an action!)
+	if ( $request_type == 'attachment' && ! SLT_CF_WP_IS_GTE_3_5 )
+		return $object;
+
+}
+
